1ef128c0ef6bf42a0a42b13cb04b2837f447ea80
shri.janani.ramesh <shri.janani.ramesh@CDC2-L-1DLWT93.dir.svc.accenture.com>
2022-05-09 13:40:33 +0530

    file added

---
+++ lib/src/main/java/Kobe/Sample.java
@@ -0,0 +1,15 @@
+package Kobe;
+
+public class Sample {
+	
+public static void main(String[] args)
+	
+	{
+    
+		System.out.println("Hello Gradle");
+		
+		System.out.println("Gradle to github test");
+		
+	}
+
+}
